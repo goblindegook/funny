45dcb8289d6fabea5c0e565263736cf6d1579354
Luis Rodrigues <goblindegook@users.noreply.github.com>
2016-11-15 16:58:20 +0000

    Test that any() is curried

--- test/any/index.js
+++ test/any/index.js
@@ -5,14 +5,18 @@ test('any() of an empty array returns false', t => {
   t.is(any(i => i, []), false)
 })
 
-test('any() of a list that fully matches the predicate function returns true', t => {
+test('any() of a list that fully matches the predicate returns true', t => {
   t.is(any(i => i, [true, true]), true)
 })
 
-test('any() of a list that partially matches the predicate function returns true', t => {
+test('any() of a list that partially matches the predicate returns true', t => {
   t.is(any(i => i, [true, false]), true)
 })
 
-test('any() of a list that never matches the predicate function returns false', t => {
+test('any() of a list that never matches the predicate returns false', t => {
   t.is(any(i => i, [false, false]), false)
+})
+
+test('any() is curried', t => {
+  t.is(any(i => i)([true, false]), true)
 })
