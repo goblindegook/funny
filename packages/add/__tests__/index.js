7f1c95a71e690240a1fd4573ca2d9c2cc526103f
Luis Rodrigues <goblindegook@users.noreply.github.com>
2016-12-05 20:24:31 +0000

    Organizing tests...

--- packages/add/__tests__/index.js
+++ packages/add/__tests__/index.js
@@ -1,19 +1,21 @@
-/* global expect it */
+/* global describe expect it */
 
 import add from '..'
 
-it('add() supports the identity property', () => {
-  expect(add(0, 3)).toBe(3)
-})
+describe('add', () => {
+  it('supports the identity property', () => {
+    expect(add(0, 3)).toBe(3)
+  })
 
-it('add() supports the commutative property', () => {
-  expect(add(2, 3)).toBe(add(3, 2))
-})
+  it('supports the commutative property', () => {
+    expect(add(2, 3)).toBe(add(3, 2))
+  })
 
-it('add() supports the associative property', () => {
-  expect(add(add(2, 3), 4)).toBe(add(2, add(3, 4)))
-})
+  it('supports the associative property', () => {
+    expect(add(add(2, 3), 4)).toBe(add(2, add(3, 4)))
+  })
 
-it('add() is curried', () => {
-  expect(add(1)(2)).toBe(3)
+  it('is curried', () => {
+    expect(add(1)(2)).toBe(3)
+  })
 })
