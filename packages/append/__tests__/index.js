e3838ec60c9759e657fcebbce59a07dccd76c3c4
Luis Rodrigues <goblindegook@users.noreply.github.com>
2017-03-22 17:54:18 +0000

    Better properties

--- packages/append/__tests__/index.js
+++ packages/append/__tests__/index.js
@@ -2,11 +2,22 @@ import jsc from 'jsverify'
 import append from '..'
 
 describe('append', () => {
-  jsc.property('insertion', 'unit', 'array', (u, a) => {
-    return jsc.utils.isEqual(append(u, a), [...a, u])
+  jsc.property('original array at the start', 'unit', 'array', (u, a) => {
+    const r = append(u, a)
+    return jsc.utils.isEqual(r.slice(0, r.length - 1), a)
   })
 
-  jsc.property('currying', 'unit', 'array', (u, a) => {
+  jsc.property('appended element at the end', 'unit', 'array', (u, a) => {
+    const r = append(u, a)
+    return jsc.utils.isEqual(r[r.length - 1], u)
+  })
+
+  jsc.property('combined length', 'unit', 'array', (u, a) => {
+    const r = append(u, a)
+    return r.length === a.length + 1
+  })
+
+  jsc.property('curry', 'unit', 'array', (u, a) => {
     return jsc.utils.isEqual(append(u)(a), append(u, a))
   })
 })
